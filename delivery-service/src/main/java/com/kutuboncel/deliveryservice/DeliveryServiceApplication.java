99ef4b5385298718821dd27a3c7c3b88b70d07a4
aprillianichristina <136817554+aprillianichristina@users.noreply.github.com>
2023-06-19 17:06:03 +0700

    Update DeliveryServiceApplication.java

--- delivery-service/src/main/java/com/kutuboncel/deliveryservice/DeliveryServiceApplication.java
+++ delivery-service/src/main/java/com/kutuboncel/deliveryservice/DeliveryServiceApplication.java
@@ -1,11 +1,12 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class DeliveryServiceApplication {
+@EnableEurekaClient
+public class ServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DeliveryServiceApplication.class, args);
+        SpringApplication.run(ServiceApplication.class, args);
     }
 }
-
