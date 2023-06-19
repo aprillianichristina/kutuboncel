d5a82bdb92f7bf96c26252e7d90e266bf928d4b1
aprillianichristina <136817554+aprillianichristina@users.noreply.github.com>
2023-06-19 17:02:50 +0700

    Update UserServiceApplication.java

--- user-service/src/main/java/com/kutuboncel/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/kutuboncel/userservice/UserServiceApplication.java
@@ -1,11 +1,12 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class UserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
 }
-
